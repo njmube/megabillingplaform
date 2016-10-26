8fcce8cca2f66c8d78ee6c4b7eb2e07965c25ccc
raykenleryh <raykenleryh@gmail.com>
2016-10-26 11:09:15 -0400

    adicionando campo enable del serie y folio

--- src/src/main/webapp/app/entities/taxpayer-account/add-seriefolio.controller.js
+++ src/src/main/webapp/app/entities/taxpayer-account/add-seriefolio.controller.js
@@ -5,17 +5,20 @@
         .module('megabillingplatformApp')
         .controller('AddSerieFolioController', AddSerieFolioController);
 
-    AddSerieFolioController.$inject = ['$scope', '$filter','$stateParams', '$uibModal', 'Taxpayer_series_folio', 'Taxpayer_account', '$uibModalInstance', '$q', 'entity', 'DataUtils', 'Tax_regime', 'C_country', 'C_state', 'C_municipality', 'C_colony', 'C_zip_code','Type_taxpayer'];
+    AddSerieFolioController.$inject = ['$scope', '$filter','AlertService', '$stateParams', '$uibModal', 'Taxpayer_series_folio', 'Taxpayer_account', '$uibModalInstance', '$q', 'entity', 'DataUtils', 'Tax_regime', 'C_country', 'C_state', 'C_municipality', 'C_colony', 'C_zip_code','Type_taxpayer'];
 
-    function AddSerieFolioController ($scope, $filter, $stateParams, $uibModal, Taxpayer_series_folio, Taxpayer_account, $uibModalInstance, $q, entity, DataUtils, Tax_regime, C_country, C_state, C_municipality, C_colony, C_zip_code, Type_taxpayer) {
+    function AddSerieFolioController ($scope, $filter, AlertService, $stateParams, $uibModal, Taxpayer_series_folio, Taxpayer_account, $uibModalInstance, $q, entity, DataUtils, Tax_regime, C_country, C_state, C_municipality, C_colony, C_zip_code, Type_taxpayer) {
         var vm = this;
 
         vm.taxpayer_account = entity;
         vm.showInfo = false;
         vm.taxpayer_series_folio = {};
+        vm.taxpayer_series_folios = [];
         var today = new Date();
         vm.toDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
         vm.messfolio = null;
+        vm.messenable = null;
+        vm.clicEdit = clicEdit;
 
         vm.add = add;
 
@@ -28,17 +31,17 @@
         function add(){
             vm.isSaving = true;
             vm.messfolio = null;
+            vm.messenable = null;
             vm.taxpayer_series_folio.taxpayer_account = vm.taxpayer_account;
             vm.taxpayer_series_folio.folio_current = vm.taxpayer_series_folio.folio_start;
             vm.taxpayer_series_folio.date_creation = vm.toDate;
-
-            if(vm.taxpayer_series_folio.folio_start <= vm.taxpayer_series_folio.folio_end ){
-                Taxpayer_series_folio.save(vm.taxpayer_series_folio, onSaveSuccess, onSaveError);
-            }
-            else{
-                vm.messfolio = 'OK';
-                vm.isSaving = false;
-            }
+                if(vm.taxpayer_series_folio.folio_start <= vm.taxpayer_series_folio.folio_end ){
+                    Taxpayer_series_folio.save(vm.taxpayer_series_folio, onSaveSuccess, onSaveError);
+                }
+                else{
+                    vm.messfolio = 'OK';
+                    vm.isSaving = false;
+                }
         }
 
         var onSaveSuccess = function (result) {
@@ -52,6 +55,27 @@
             vm.isSaving = false;
         };
 
+        function clicEdit(){
+            if(vm.taxpayer_series_folio.enable == true){
+                vm.taxpayer_series_folios = Taxpayer_series_folio.query({
+                    taxpayeraccount: vm.taxpayer_account.id
+                },onSuccess, onError);
+            }
+        }
+
+        function onSuccess(data) {
+            vm.taxpayer_series_folios = data;
+
+            for(var i = 0; i < vm.taxpayer_series_folios.length;i++){
+                if(vm.taxpayer_series_folios[i].enable){
+                    vm.messenable = 'OK';
+                    vm.taxpayer_series_folio.enable = false;
+                }
+            }
+        }
+        function onError(error) {
+            AlertService.error(error.data.message);
+        }
 
         vm.cancel = function () {
             $uibModalInstance.dismiss('cancel');
--- src/src/main/webapp/app/entities/taxpayer-account/dialog-seriefolio.controller.js
+++ src/src/main/webapp/app/entities/taxpayer-account/dialog-seriefolio.controller.js
@@ -5,15 +5,17 @@
         .module('megabillingplatformApp')
         .controller('DialogSerieFolioController', DialogSerieFolioController);
 
-    DialogSerieFolioController.$inject = ['$scope', '$filter','$stateParams', '$uibModal', 'Taxpayer_series_folio', 'Taxpayer_account', '$uibModalInstance', '$q', 'entity', 'DataUtils', 'Tax_regime', 'C_country', 'C_state', 'C_municipality', 'C_colony', 'C_zip_code','Type_taxpayer'];
+    DialogSerieFolioController.$inject = ['$scope', '$filter','AlertService', '$stateParams', '$uibModal', 'Taxpayer_series_folio', 'Taxpayer_account', '$uibModalInstance', '$q', 'entity', 'DataUtils', 'Tax_regime', 'C_country', 'C_state', 'C_municipality', 'C_colony', 'C_zip_code','Type_taxpayer'];
 
-    function DialogSerieFolioController ($scope, $filter, $stateParams, $uibModal, Taxpayer_series_folio, Taxpayer_account, $uibModalInstance, $q, entity, DataUtils, Tax_regime, C_country, C_state, C_municipality, C_colony, C_zip_code, Type_taxpayer) {
+    function DialogSerieFolioController ($scope, $filter, AlertService, $stateParams, $uibModal, Taxpayer_series_folio, Taxpayer_account, $uibModalInstance, $q, entity, DataUtils, Tax_regime, C_country, C_state, C_municipality, C_colony, C_zip_code, Type_taxpayer) {
         var vm = this;
 
         vm.taxpayer_series_folio = entity;
         vm.showInfo = false;
         vm.add = add;
         vm.messfolio = null;
+        vm.messenable = null;
+        vm.clicEdit = clicEdit;
 
         function add(){
             vm.isSaving = true;
@@ -37,6 +39,28 @@
         var onSaveError = function () {
             vm.isSaving = false;
         };
+
+        function clicEdit(){
+            if(vm.taxpayer_series_folio.enable == true){
+                vm.taxpayer_series_folios = Taxpayer_series_folio.query({
+                    taxpayeraccount: vm.taxpayer_series_folio.taxpayer_account.id
+                },onSuccess, onError);
+            }
+        }
+
+        function onSuccess(data) {
+            vm.taxpayer_series_folios = data;
+
+            for(var i = 0; i < vm.taxpayer_series_folios.length;i++){
+                if(vm.taxpayer_series_folios[i].enable && vm.taxpayer_series_folio.id != vm.taxpayer_series_folios[i].id){
+                    vm.messenable = 'OK';
+                    vm.taxpayer_series_folio.enable = false;
+                }
+            }
+        }
+        function onError(error) {
+            AlertService.error(error.data.message);
+        }
 
 
         vm.cancel = function () {
