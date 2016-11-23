5a13bff40a5a38f2127d0ea9ceb6c07451482add
Yandry Alberto Terry <yalberto@gmail.com>
2016-11-23 12:06:40 -0500

    Subiendo arreglos en el expirar session. Se incluyo un tipo de evento.

--- src/src/main/java/org/megapractical/billingplatform/security/CustomAccessDeniedHandler.java
+++ src/src/main/java/org/megapractical/billingplatform/security/CustomAccessDeniedHandler.java
@@ -2,11 +2,17 @@ package org.megapractical.billingplatform.security;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.megapractical.billingplatform.domain.Audit_event_type;
+import org.megapractical.billingplatform.domain.C_state_event;
+import org.megapractical.billingplatform.service.Audit_event_typeService;
+import org.megapractical.billingplatform.service.C_state_eventService;
+import org.megapractical.billingplatform.service.TracemgService;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.AccessDeniedHandlerImpl;
@@ -26,8 +32,25 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     private AccessDeniedHandlerImpl accessDeniedHandlerImpl = new AccessDeniedHandlerImpl();
 
+    @Inject
+    private Audit_event_typeService audit_event_typeService;
+
+    @Inject
+    private C_state_eventService c_state_eventService;
+
+    @Inject
+    private TracemgService tracemgService;
+
     public void handle(HttpServletRequest request, HttpServletResponse response,
             AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        Long idauditevent = new Long("53");
+        Audit_event_type audit_event_type = audit_event_typeService.findOne(idauditevent);
+
+        Long idstate = new Long("1");
+        C_state_event c_state_event = c_state_eventService.findOne(idstate);
+
+        tracemgService.saveTraceUser(SecurityUtils.intentLogin,audit_event_type,c_state_event);
 
         if (accessDeniedException instanceof CsrfException && !response.isCommitted()) {
             // Remove the session cookie so that client knows it's time to obtain a new CSRF token
